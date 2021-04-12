db5ca7baaefc8900cc4c7da338b10093d5692389
JOON <JOON@DESKTOP-3OQCUOK>
2021-04-12 16:38:17 +0900

    twice

--- test/src/test/Dedt.java
+++ test/src/test/Dedt.java
@@ -4,7 +4,10 @@ public class Dedt {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		int num1 = 100;
+		int num = 30;
+		System.out.println("hello java");
+		num+=num1;
 	}
 
 }
